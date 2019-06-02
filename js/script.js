28afb4f3a3966f45e408af11d5376196bd5a75a8
lopezcarlos47 <carlosignacio300lopez@gmail.com>
2019-06-01 18:32:15 -0600

    tercer cambio

--- js/script.js
+++ js/script.js
@@ -1,13 +1,14 @@
-var pokemon=angular.module("pokemon",[])
-pokemon.controller("pokedex",function($scope,$rootscope,$http){
-	$scope.pokedex = {};
-
-	for (var i = 0; i < 100; i++) {
-	http({
-		method: "GET",
-		url: "https://pokeapi.co/api/v2/pokemon/"+i
-	}).the(function callbacksuccess(y){
-	console.log(y)
-     })
-  }
-})
+// Write your code here
+var Pokemon = angular.module("Pokemon",[])
+Pokemon.controller("pokedex",function($scope,$rootScope,$http){
+	$scope.pokedexB = [];
+	for(var y=1;y<=100;y++){
+		$http({
+		method:"GET",
+		url:"https://pokeapi.co/api/v2/pokemon/"+y
+	}).then(function pokedex(x){
+		$scope.pokedexB.push(x);
+	})
+	}
+	
+});
